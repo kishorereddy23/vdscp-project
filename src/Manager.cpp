4cf4ef3add93bdd01f19955cdef6cf89a83c9523
kishorereddy23 <venkatakishorepakala@gmail.com>
2025-12-03 23:01:13 +0100

    TDD: break createVar to start from failing CreateVariable test

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -32,10 +32,10 @@ namespace ClassProject {
     }
 
     BDD_ID Manager::createVar(const std::string &label) {
-        BDD_ID newId = uniqueTable.size();
-        uniqueTable.emplace_back(newId, trueId, falseId, newId, label);
-        return newId;
-    }
+    // temporary wrong implementation for TDD
+    return 0;
+}
+
 
     BDD_ID Manager::topVar(BDD_ID f) {
         return uniqueTable[f].topVar;
