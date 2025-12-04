f267300ec22c7168177d6df527e421c9333aea3f
Kishore <kishorereddy.pakala@edu.rptu.de>
2025-12-04 01:56:24 +0100

    TDD: break xor2 to see XorWithSelf test fail

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -150,8 +150,9 @@ namespace ClassProject {
 
 
     BDD_ID Manager::xor2(BDD_ID a, BDD_ID b) {
-        return ite(a, neg(b), b);
+    return 0;  // broken
     }
+
 
     BDD_ID Manager::neg(BDD_ID a) {
     return ite(a, falseId, trueId);
