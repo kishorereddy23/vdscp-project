6b101069ebbf7d008f28a363ea2c4a09af894d98
Kishore <kishorereddy.pakala@edu.rptu.de>
2025-12-04 01:21:59 +0100

    TDD: implement coFactorTrue to satisfy CoFactorTrue test

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -73,9 +73,15 @@ namespace ClassProject {
     }
 
     BDD_ID Manager::coFactorTrue(BDD_ID f, BDD_ID x) {
-    // broken for TDD
-    return 0;
-}
+    if (isConstant(f)) return f;
+    if (topVar(f) != x) {
+        BDD_ID highCofactor = coFactorTrue(uniqueTable[f].high, x);
+        BDD_ID lowCofactor = coFactorTrue(uniqueTable[f].low, x);
+        return findOrCreateNode(highCofactor, lowCofactor, topVar(f));
+    }
+    return uniqueTable[f].high;
+    }
+
 
 
     BDD_ID Manager::coFactorFalse(BDD_ID f, BDD_ID x) {
